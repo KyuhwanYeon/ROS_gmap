8f72988c4d54f7c5ab441fba8c49891af1cbe06f
KyuhwanYeon <KyuhwanYeon.gmail.com>
2020-02-08 22:14:34 +0900

    initial upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/khyeon/workspace/013_FastSLAM/src
+# Build directory: /home/khyeon/workspace/013_FastSLAM/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
+subdirs(slam_gmapping/slam_gmapping)
+subdirs(teleop_twist_keyboard)
+subdirs(ball_chaser)
+subdirs(slam_gmapping/gmapping)
---
+++ build/slam_gmapping/slam_gmapping/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/khyeon/workspace/013_FastSLAM/src/slam_gmapping/slam_gmapping
+# Build directory: /home/khyeon/workspace/013_FastSLAM/build/slam_gmapping/slam_gmapping
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
